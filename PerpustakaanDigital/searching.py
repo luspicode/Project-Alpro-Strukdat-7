117a515b4b831f699cbfdf6cc083da1fb854b411
hafasrevan <hafastreifan@gmail.com>
2026-05-18 02:46:41 +0700

    update searching

---
+++ PerpustakaanDigital/searching.py
@@ -0,0 +1,40 @@
+def linear_search(daftar_buku, keyword):
+
+    keyword = keyword.lower()
+
+    for buku in daftar_buku:
+
+        if keyword in buku["judul"].lower():
+            return buku
+
+    return None
+
+def binary_search(daftar_buku, keyword):
+
+    low = 0
+    high = len(daftar_buku) - 1
+
+    keyword = keyword.lower()
+
+    while low <= high:
+
+        mid = (low + high) // 2
+
+        judul_tengah = (
+            daftar_buku[mid]["judul"]
+            .lower()
+        )
+
+        # Buku ditemukan
+        if keyword == judul_tengah:
+            return daftar_buku[mid]
+
+        # Cari ke kanan
+        elif keyword > judul_tengah:
+            low = mid + 1
+
+        # Cari ke kiri
+        else:
+            high = mid - 1
+
+    return None
