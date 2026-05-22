e45b0d294d79c8a06f4ab2a5fe487ec0fed874ab
luspicode <krishnaharyadiluthfi@gmail.com>
2026-05-22 13:30:28 +0700

    build stack

--- PerpustakaanDigital/stack.py
+++ PerpustakaanDigital/stack.py
@@ -1,2 +1,12 @@
-def Stack():
-    pass
+class Stack:
+    def __init__(self):
+        self.data = []
+
+    def push(self, item):
+        self.data.append(item)
+
+    def pop(self):
+        return self.data.pop()
+
+    def is_empty(self):
+        return len(self.data) == 0
