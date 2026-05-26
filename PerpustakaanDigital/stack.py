cd33fb4ff92bc6288a3884a88919f1e270229841
hafasrevan <hafastreifan@gmail.com>
2026-05-26 19:58:17 +0700

    Update stack.py

--- PerpustakaanDigital/stack.py
+++ PerpustakaanDigital/stack.py
@@ -1,2 +1,22 @@
-def Stack():
-    pass
+class Stack:
+    def __init__(self):
+        self.data = []
+
+    def push(self, item):
+        self.data.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.data.pop()
+        return None
+
+    def is_empty(self):
+        return len(self.data) == 0
+
+    def peek(self):
+        if not self.is_empty():
+            return self.data[-1]
+        return None
+
+    def size(self):
+        return len(self.data)
